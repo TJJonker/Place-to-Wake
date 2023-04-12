fd3fad542961b04b4c2e8e4fd34262d1ebffd0aa
Tom Jonker <60851185+TJJonker@users.noreply.github.com>
2023-04-12 20:53:04 +0200

    Project commit

---
+++ app/src/main/java/nl/TwoDots/placetowake/MainActivity.kt
@@ -0,0 +1,43 @@
+package nl.TwoDots.placetowake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import nl.TwoDots.placetowake.ui.theme.PlaceToWakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlaceToWakeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PlaceToWakeTheme {
+        Greeting("Android")
+    }
+}
